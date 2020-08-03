3b117ae251c4a2cfe5bd18f6abbae849638d93ca
Kamil <40772107+baldir0@users.noreply.github.com>
2020-08-03 22:35:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+<HTML>
+    <HEAD>
+        <meta charset='utf-8'/>
+        <title>3Pixel</title>
+        <link type="text/css" href="./styles/main.css" rel="stylesheet">
+        <link type="text/css" href="./styles/home.css" rel="stylesheet">
+        <link rel="icon" href="./img/logo.png">
+    </HEAD>
+    <BODY>
+        <div id="container">
+            <div id="nav-bar">
+                <nav>
+                    <div class="logo">
+                        <img src="./img/logo.png" alt="Image Logo">
+                    </div>
+                    <ul>
+                        <li><a href="#top">Home</a></li>
+                        <li><a href="#news-container">News</a></li>
+                        <li><a href="#about-us">About Us</a></li>
+                        <div id="sign-in-button">
+                            <li><a href="">Sign In</a></li>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
+            <div id="main-container">
+                <div id="ImageBoard">
+                    <div class='ImageBoardImage'>
+
+                    </div>
+                    <div id="ImageBoardButtons">
+                        <button id="Img1"></button>
+                        <button id="Img2"></button>
+                        <button id="Img3"></button>
+                        <button id="Img4"></button>
+                    </div>
+                </div>
+                <div id="about-us">
+                    <h3 id="about-us-title">About Us</h3>
+                    <p id="about-us-text">We are group of people starting to build fast games or flash project games.
+                    We are working in that games to filter out people we cant relay on and with people we can relay on start working bigger better and more stable games thoward building one day company and stay as employs. 
+                    As of now on game release we will all gain portfolio and letter of reccomendation by me.</p>
+                </div>
+                <div id="news-container">
+                    <h3 id="news-title">News</h3>
+                    <div class="news">
+                        <div id="title-container">
+                            <h2 id="title">Title</h2>
+                            <span id="date">Author: author<br>Date:22.02.2002</span>
+                        </div>
+                        <div id="news-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas turpis enim, mattis at orci non, blandit faucibus dui. Cras ullamcorper ac nisl pulvinar tristique. Morbi mattis justo
+ sed lectus blandit ultricies. Suspendisse a purus turpis. Maecenas fringilla porttitor laoreet. Vestibulum quam ex, viverra quis rhoncus id, varius sed nisl. Quisque vitae luctus massa. 
+Nunc commodo tortor nec tincidunt eleifend. Aenean non ultricies ante, nec tincidunt mauris. Phasellus tincidunt porta magna, sit amet rhoncus ligula luctus quis. Nunc turpis eros, 
+vehicula feugiat nibh ut, scelerisque lacinia lectus.</p>
+                        </div>                    
+                    </div>
+                    <div class="news">
+                        <div id="title-container">
+                            <h2 id="title">Title</h2>
+                            <span id="date">Author: author<br>Date:22.02.2002</span>
+                        </div>
+                        <div id="news-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas turpis enim, mattis at orci non, blandit faucibus dui. Cras ullamcorper ac nisl pulvinar tristique. Morbi mattis justo
+ sed lectus blandit ultricies. Suspendisse a purus turpis. Maecenas fringilla porttitor laoreet. Vestibulum quam ex, viverra quis rhoncus id, varius sed nisl. Quisque vitae luctus massa. 
+Nunc commodo tortor nec tincidunt eleifend. Aenean non ultricies ante, nec tincidunt mauris. Phasellus tincidunt porta magna, sit amet rhoncus ligula luctus quis. Nunc turpis eros, 
+vehicula feugiat nibh ut, scelerisque lacinia lectus.</p>
+                        </div>                    
+                    </div>
+                    <div class="news">
+                        <div id="title-container">
+                            <h2 id="title">Title</h2>
+                            <span id="date">Author: author<br>Date:22.02.2002</span>
+                        </div>
+                        <div id="news-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas turpis enim, mattis at orci non, blandit faucibus dui. Cras ullamcorper ac nisl pulvinar tristique. Morbi mattis justo
+ sed lectus blandit ultricies. Suspendisse a purus turpis. Maecenas fringilla porttitor laoreet. Vestibulum quam ex, viverra quis rhoncus id, varius sed nisl. Quisque vitae luctus massa. 
+Nunc commodo tortor nec tincidunt eleifend. Aenean non ultricies ante, nec tincidunt mauris. Phasellus tincidunt porta magna, sit amet rhoncus ligula luctus quis. Nunc turpis eros, 
+vehicula feugiat nibh ut, scelerisque lacinia lectus.</p>
+                        </div>                    
+                    </div>
+                <?php
+                    //Here Will Be 3 Last News
+                ?>
+                </div>
+            </div>
+            <div id="footer">
+                <footer>
+                    <span>© 2020 - 3 Pixel Team</span>
+                    <div class="logo">
+                        <img src="./img/logo.png" alt="img">
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </BODY>
+</HTML>
